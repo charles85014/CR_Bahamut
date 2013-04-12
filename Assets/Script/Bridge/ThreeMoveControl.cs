fe098ce189e8634124d41efa29f96cdd6558f59d
Charles-Wang <charles85014@livemail.tw>
2013-04-12 12:01:29 +0800

    four  notyet

--- Assets/Script/Bridge/ThreeMoveControl.cs
+++ Assets/Script/Bridge/ThreeMoveControl.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ThreeMoveControl : MonoBehaviour {
     public GameObject[] MoveConveyor = new GameObject[3];
-    
-    public float[] phidgetvalue;
+
+    public float ConveyorSpeed;
     public float Move01_Height, Move01_Low;
     public float Move02_Height, Move02_Low;
     public float Move03_Height, Move03_Low;
@@ -15,10 +15,50 @@ public class ThreeMoveControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-        MoveConveyor[0].transform.position = new Vector3(MoveConveyor[0].transform.position.x, MoveConveyor[0].transform.position.y, Move01_Height - phidgetvalue[0] / 999 * Move01_Low);
-        MoveConveyor[1].transform.position = new Vector3(MoveConveyor[1].transform.position.x, MoveConveyor[1].transform.position.y, Move02_Height - phidgetvalue[1] / 999 * Move02_Low);
-        MoveConveyor[2].transform.position = new Vector3(MoveConveyor[2].transform.position.x, MoveConveyor[2].transform.position.y, Move03_Height - phidgetvalue[2] / 999 * Move03_Low);
-	}
+        if (MoveConveyor[0].transform.position.z < Move01_Height)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                MoveConveyor[0].transform.position = new Vector3(MoveConveyor[0].transform.position.x, MoveConveyor[0].transform.position.y, MoveConveyor[0].transform.position.z + ConveyorSpeed * Time.deltaTime);
+            }
+        }
+        if (MoveConveyor[0].transform.position.z > Move01_Low)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                MoveConveyor[0].transform.position = new Vector3(MoveConveyor[0].transform.position.x, MoveConveyor[0].transform.position.y, MoveConveyor[0].transform.position.z - ConveyorSpeed * Time.deltaTime);
+            }
+        }
+
+        if (MoveConveyor[1].transform.position.z < Move02_Height)
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                MoveConveyor[1].transform.position = new Vector3(MoveConveyor[1].transform.position.x, MoveConveyor[1].transform.position.y, MoveConveyor[1].transform.position.z + ConveyorSpeed * Time.deltaTime);
+            }
+        }
+        if (MoveConveyor[1].transform.position.z > Move02_Low)
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                MoveConveyor[1].transform.position = new Vector3(MoveConveyor[1].transform.position.x, MoveConveyor[1].transform.position.y, MoveConveyor[1].transform.position.z - ConveyorSpeed * Time.deltaTime);
+            }
+        }
+
+        if (MoveConveyor[2].transform.position.z < Move03_Height)
+        {
+            if (Input.GetKey(KeyCode.H))
+            {
+                MoveConveyor[2].transform.position = new Vector3(MoveConveyor[2].transform.position.x, MoveConveyor[2].transform.position.y, MoveConveyor[2].transform.position.z + ConveyorSpeed * Time.deltaTime);
+            }
+        }
+        if (MoveConveyor[2].transform.position.z > Move03_Low)
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                MoveConveyor[2].transform.position = new Vector3(MoveConveyor[2].transform.position.x, MoveConveyor[2].transform.position.y, MoveConveyor[2].transform.position.z - ConveyorSpeed * Time.deltaTime);
+            }
+        }
+    }
    
 }
